274ce32e729d5aa352ba0550a442e26472c89c56
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2022-03-10 09:51:01 +0700

    Create latihan13.php
    add latihan 13

---
+++ php/latihan13.php
@@ -0,0 +1,18 @@
+<?php
+$cars = array
+	(
+	array("volvo",22,18),
+	array("BMW",15,13),
+	array("Saab",5,2),
+	array("Land Rover",17,15)
+	);
+	
+for ($row = 0; $row < 4; $row++) {
+	echo "<p><b>ROW number $row</b></p>";
+	echo "<ul>";
+	for ($col = 0; $col < 3; $col++) {
+	echo"<li>".$cars[$row][$col]."</li>";
+	}
+	echo "</ul>";
+}
+?>
