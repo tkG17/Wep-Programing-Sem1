43714d135ec50a18b63806d10797d73a90751e97
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2022-03-11 10:14:15 +0700

    Create latihan19_menampilkan.php
    add tampil html

---
+++ php/latihan19_menampilkan.php
@@ -0,0 +1,9 @@
+<?php
+// include header
+include "latihan19_header.php";
+// include konten 
+include "latihan19_konten.php";
+// include copyright
+include "latihan19_copyright.php";
+
+//folder ini dapat di gunakan jika digabungkan dengan latihan yang ada di file html
